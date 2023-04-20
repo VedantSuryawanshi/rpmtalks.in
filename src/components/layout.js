c93e131002454e06a52d0668e81d249a8a626a04
vedantsuryawanshi <hellovedantsuryawanshi@gmail.com>
2023-04-20 16:05:21 +0530

    commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} RPMTALKS &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://ved.wtf">Vedant Surwayanshi</a></p>
+        <p>&copy; {new Date().getFullYear()} RPMTALKS &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://instagram.com/thevedantsuryawanshi">Vedant Surwayanshi</a></p>
       </footer>
     </div>
   )
